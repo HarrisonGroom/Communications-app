f6a9fc8fba624d03c43800a4b15c09a929a25f5e
HarrisonGroom <89763078+HarrisonGroom@users.noreply.github.com>
2022-04-20 15:18:03 -0600

    text to speech trying to get it to work

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
-
+  constructor(private tts:  TextToSpeech) { this.tts.speak('Hello World')
+  .then(() => console.log('Success'))
+  .catch((reason: any) => console.log(reason));
+    
+  }
+ 
 }
