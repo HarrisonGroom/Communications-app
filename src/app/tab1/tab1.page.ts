654fb6d3d41c39d3d918a975c92ae150d3a05516
HarrisonGroom <89763078+HarrisonGroom@users.noreply.github.com>
2022-04-28 11:19:02 -0600

    alerts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,52 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor( public alertController: AlertController) {}
+
+  sshowAlert() {
+
+    this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle for alert',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    }).then(res => {
+
+      res.present();
+
+    });
+
+  }
+
+  showConfirm() {
+    this.alertController.create({
+      header: 'Confirm Alert',
+      subHeader: 'Beware lets confirm',
+      message: 'Are you sure? you want to leave without safty mask?',
+      buttons: [
+        {
+          text: 'Never',
+          handler: () => {
+            console.log('I care about humanity');
+          }
+        },
+        {
+          text: 'Not Sure',
+          handler: () => {
+            console.log('Let me think');
+          }
+        },
+        {
+          text: 'Yes!',
+          handler: () => {
+            console.log('Whatever');
+          }
+        }
+      ]
+    }).then(res => {
+      res.present();
+    });
+  }
+
 
 }
